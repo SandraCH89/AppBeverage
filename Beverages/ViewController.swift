59af8b0032c4a44a0fea9c5c736470a00cc64c5a
Sandra <s.a.camunas.heredia@accenture.com>
2020-04-28 14:40:02 +0200

    Cmmit Initial

---
+++ Beverages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beverages
+//
+//  Created by Sandra Camuñas Heredia on 28/04/2020.
+//  Copyright © 2020 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
